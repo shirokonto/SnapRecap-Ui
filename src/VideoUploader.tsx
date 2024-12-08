46c6479077cb795a48e48aab0ceec62d228a8a94
Shiro <hikari.hanakimi@gmail.com>
2024-12-08 19:10:17 +0100

    improve confluence api calls

--- src/VideoUploader.tsx
+++ src/VideoUploader.tsx
@@ -7,21 +7,16 @@ type Result = {
 };
 
 const VideoUploader = () => {
+    const [file, setFile] = useState<File | null>(null);
     const [fileName, setFileName] = useState('');
     const [transcription, setTranscription] = useState('');
     const [summary, setSummary] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [sections, setSections] = useState<string[]>(['']);
+    const [confluencePageId, setConfluencePageId] = useState('');
 
-    const confluenceBaseUrl = process.env.REACT_APP_CONFLUENCE_BASE_URL;
-    const parentPageId = process.env.REACT_APP_CONFLUENCE_PARENT_PAGE_ID;
-    const spaceKey = process.env.REACT_APP_CONFLUENCE_SPACEKEY;
-    const confluenceToken = process.env.REACT_APP_CONFLUENCE_TOKEN;
-
-    const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (!event.target.files) return;
-
-        const file = event.target.files[0];
+    const handleGetSummary = async () => {
+        if (!file) return; // Ensure file is uploaded
         const formData = new FormData();
         formData.append('file', file);
         formData.append('sections', JSON.stringify(sections));
@@ -35,15 +30,78 @@ const VideoUploader = () => {
                 mode: 'cors',
             });
             const result: Result = await response.json();
-            console.log("result", result);
             setFileName(result.file_name);
             setTranscription(result.transcription);
             setSummary(result.summary);
         } catch (error) {
-            console.error('Error uploading video:', error);
+            console.error("Error uploading video:", error);
         } finally {
             setIsLoading(false);
         }
+    }
+
+    const handleCreateNewOnConfluence = async () => {
+        if (!confluencePageId || confluencePageId === '') {
+            console.error("Parent page ID is not set");
+            return;
+        }
+
+        try {
+            const formData = new FormData();
+            formData.append('parent_id', confluencePageId);
+            formData.append('title', "Test Page Py");
+            formData.append('content', `<h1>This is a second test page</h1><p>This is a test page with py</p><img src="https://picsum.photos/200/300" alt="Example Image" />`);
+
+            const response = await fetch("http://localhost:8000/postonconfluence", {
+                method: "POST",
+                body: formData,
+                mode: "cors",
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                console.log("Page created successfully:", result);
+            } else {
+                const errorText = await response.text();
+                console.error("Failed to create page:", response.status, errorText);
+            }
+        } catch (error) {
+            console.error("Error while creating page:", error);
+        }
+    };
+
+    const handleUpdateOnConfluence = async () => {
+        if (!confluencePageId || confluencePageId === '') {
+            console.error("Parent page ID is not set");
+            return;
+        }
+        try {
+            const formData = new FormData();
+            formData.append("title", "Update page test");
+            formData.append("content", "<h1>This is a update test page</h1><p>This is an updated test page with py</p><p>Test Nr 4</p><img src=\"https://picsum.photos/200/300\" alt=\"Example Image\" />");
+            formData.append("page_id", confluencePageId);
+
+            const response = await fetch("http://localhost:8000/updateonconfluence", {
+                method: "PUT",
+                body: formData,
+                mode: "cors",
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                console.log("Page updated successfully:", result);
+            } else {
+                const errorText = await response.text();
+                console.error("Failed to update page:", response.status, errorText);
+            }
+        } catch (error) {
+            console.error("Error while updating page:", error);
+        }
+    };
+
+    const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (!event.target.files) return;
+        setFile(event.target.files[0]);
     };
 
     const handleSectionChange = (index: number, value: string) => {
@@ -61,78 +119,12 @@ const VideoUploader = () => {
         setSections(updatedSections);
     };
 
-    const handlePublishOnConfluence = async () => {
-        try {
-            const response = await fetch(`${confluenceBaseUrl}/rest/api/content/${parentPageId}`, {
-                method: "PUT",
-                headers: {
-                    "Authorization": `Bearer ${confluenceToken}`,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    version: { number: 2 },
-                    title: `Summary for Test`,
-                    type: "page",
-                    space: {
-                        key: spaceKey
-                    },
-                    body: {
-                        storage: {
-                            "value": "<h1>Your Content Here</h1>",
-                            representation: "storage",
-                        },
-                    },
-                }),
-            });
-
-            if (response.ok) {
-                const result = await response.json();
-                console.log("Successfully published to Confluence:", result);
-            } else {
-                const errorText = await response.text();
-                console.error("Failed to publish to Confluence:", response.status, errorText);
-            }
-        } catch (error) {
-            console.error("Error while publishing to Confluence:", error);
+    const handlePageIdChange = (pageId: string) => {
+        if (pageId) {
+            console.log("Page ID:", pageId);
+            setConfluencePageId(pageId);
         }
-    };
-
-    // TODO works with Postman - here CORS error
-    const handleCreateNewOnConfluence = async () => {
-        try {
-            const response = await fetch(`${confluenceBaseUrl}/rest/api/content`, {
-                method: "POST",
-                headers: {
-                    "Authorization": `Bearer ${confluenceToken}`,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    type: "page",
-                    title: "React Test Page",
-                    space: { key: spaceKey },
-                    ancestors: [
-                        { id: parentPageId }
-                    ],
-                    body: {
-                        storage: {
-                            value: `<h1>Welcome</h1><p>This is the content of the new page.</p>`,
-                            representation: "storage",
-                        },
-                    },
-                }),
-            });
-
-            if (response.ok) {
-                const result = await response.json();
-                console.log("Page created successfully:", result);
-            } else {
-                const errorText = await response.text();
-                console.error("Failed to create page:", response.status, errorText);
-            }
-        } catch (error) {
-            console.error("Error while creating page:", error);
-        }
-    };
+    }
 
     return (
         <div>
@@ -155,7 +147,8 @@ const VideoUploader = () => {
                 ))}
                 <button onClick={addSection}>Add Section</button>
             </div>
-
+            <br/>
+            <button onClick={handleGetSummary} disabled={!file}>Get Summary</button>
             <div>
                 <h3>Subtitle File Name:</h3>
                 <p>{fileName}</p>
@@ -165,8 +158,16 @@ const VideoUploader = () => {
                     <div>
                         <h3>Summary:</h3>
                         <p>{summary}</p>
-                        <button onClick={() => handlePublishOnConfluence()}>Publish</button>
-                        <button onClick={() => handleCreateNewOnConfluence()}>Create new</button>
+                        <div>
+                            <input type="text"
+                                   value={confluencePageId}
+                                   placeholder="Confluence Page ID"
+                                   onChange={(e) => handlePageIdChange(e.target.value)}/>
+                            <br/>
+                            <button onClick={() => handleUpdateOnConfluence()}>Update on given page</button>
+                            <br/>
+                            <button onClick={() => handleCreateNewOnConfluence()}>Create new under given parent id</button>
+                        </div>
 
                         <h3>Transcription:</h3>
                         <pre style={{ whiteSpace: 'pre-wrap' }}>{transcription}</pre>
