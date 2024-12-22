d93bd354f93eb58a1e1959e1787e8e463b820ac3
Shiro <hikari.hanakimi@gmail.com>
2024-12-22 13:23:16 +0100

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+/**https://tailwindcss.com/docs/guides/create-react-app */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
